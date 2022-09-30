e2f3f6a1a8a3d4587037e3cbf3e00d809baabf16
guillehub <guillejp98@gmail.com>
2022-09-30 17:13:46 +0200

    ejerciciosphp

---
+++ tema1/ejer2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    $cadena1 ="hola a todo el mundo ";
+    $cadena2= "mi nombre es guillermo jabalera";
+
+    echo $cadena3 = $cadena1.$cadena2;
+    
+    
+    ?>
+</body>
+</html>
