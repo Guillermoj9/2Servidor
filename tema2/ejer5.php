1320ce31ba1255fc96ea469c16bd9fa8c2d4dbda
guillehub <guillejp98@gmail.com>
2022-10-22 20:18:04 +0200

    final

--- tema2/ejer5.php
+++ tema2/ejer5.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <?php
+    
+<?php
 
-    function encriptar($mensaje, $clave)
+function encriptar($mensaje, $clave)
     {
 
+        //Le damos la vuelta al mensaje con STRREV.
+        $mensajeR = strrev($mensaje);
+        echo $mensajeR;
+        echo "<br>";
         //Convierte string en array.
-        $letras = str_split($mensaje, 1);
-
+        $letras = str_split($mensaje,1);
         foreach ($letras as $valor) {
             //Convierte el primer byte de un string a un valor entre 0 y 255.
             $conv = ord($valor);
@@ -31,6 +33,10 @@
 
     function desencriptar($mensaje, $clave)
     {
+         //Le damos la vuelta al mensaje con STRREV.
+        $mensajeR = strrev($mensaje);
+        echo $mensajeR;
+        echo "<br>";
         //Convierte string en array.
         $letras = str_split($mensaje, 1);
 
@@ -45,7 +51,9 @@
 
     echo encriptar("Todoloquetengoenmividasonmis...ymipalabra", 3);
     echo desencriptar("Wrgrortxhwhqjrhqplylgdvrqplv111|plsdodeud",3);
-    ?>
-</body>
 
+
+?>
+
+</body>
 </html>
