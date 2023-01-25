3cb01b645bf8d863e153d931763e0aa056b4de51
guillehub <guillejp98@gmail.com>
2023-01-25 17:19:33 +0100

    20202

--- tema8/recuperacion/modelo/ConexionBD.php
+++ tema8/recuperacion/modelo/ConexionBD.php
@@ -1,7 +1,7 @@
 <?php
 
 use MongoDB\Client;
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 class ConexionBD {
 
@@ -13,7 +13,6 @@ class ConexionBD {
             //CONEXIÓN A MONGODB CLOUD ATLAS. Comentar esta línea para conectar en local.
             //$host = "mongodb+srv://admin:evhT1Hu8ZasF8llx@cluster0.qmwhh.mongodb.net/".$database."?retryWrites=true&w=majority";
            $host =  "mongodb+srv://guille:1234@cluster0.zhq5qyh.mongodb.net/steam";
-            //$host = "mongodb://root:toor@mongo:27017/"; //MongoDB en Docker
             self::$conexion = (new Client($host))->{$bd};
         } catch (Exception $e){
             echo $e->getMessage();
